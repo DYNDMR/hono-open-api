e87e4b3db2b9e06b8c328a198846ce7ca390052e
Dayane <dayane.rodrigues@bcrcx.com>
2024-12-12 22:07:18 -0300

    feat: setup database connection with drizzle

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import configureOpenAPI from "@/lib/configure-open-api";
 import createApp from "@/lib/create-app";
 import index from "@/routes/index.route";
+import tasks from "@/routes/tasks/tasks.index";
 
 const app = createApp();
 
@@ -8,6 +9,7 @@ configureOpenAPI(app);
 
 const routes = [
   index,
+  tasks,
 ] as const;
 
 routes.forEach((route) => {
